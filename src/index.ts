728c15b3c16a20b43bef777ffe2c4cdc13af766f
Josh Cox <cox.josh.d@gmail.com>
2018-09-21 00:18:30 -0500

    Update el : (string, Attributes, HTMLElement[]) -> HTMLElement function

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,11 @@ interface Attributes {
 /**
  * Generate a DOM Element
  */
-const el = (tag: string, attributes: Attributes) =>
-    Object.keys(attributes).reduce((e: HTMLElement, key: string): HTMLElement => {
+const el = (tag: string, attributes: Attributes, children: HTMLElement[]) =>
+    children.reduce((e: HTMLElement, child: HTMLElement): HTMLElement => {
+        e.appendChild(child);
+        return e;
+    }, Object.keys(attributes).reduce((e: HTMLElement, key: string): HTMLElement => {
         e.setAttribute(key, attributes[key]);
         return e;
-    }, document.createElement(tag));
+    }, document.createElement(tag)));
